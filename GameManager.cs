044fa6d0644a7560707c62a55e0775c1265d64bf
altman0828 <wsb0828@naver.com>
2016-10-03 20:08:01 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Player p1 = new Player(); //플레이하는 플레이어
+    public Player p2 = new Player(); //상대방
+
+    void Start () {
+	    if(isFirst())
+        {
+            p1.goFirst("p1");
+            p2.goSnd("p2");
+        }
+        else
+        {
+            p2.goFirst("p2");
+            p1.goSnd("p1");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TurnCount();
+	}
+
+    //선공 후공을 정하는 메소드
+    bool isFirst ()
+    {
+        float rand = Random.Range(0f, 1.0f);
+        if (rand > 0.5)
+            return true;
+        else
+            return false;
+    }
+
+    //턴의 시간제한
+    void TurnCount ()
+    {
+        float left_time = 1.0f - Time.deltaTime;
+        if (left_time <= 0)
+            changeTurn();
+        
+    }
+
+    public static void changeTurn()
+    {
+        Debug.Log("Turn Changed");
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+    public int health;
+    
+	public Player ()
+    {
+        health = 30;
+	}
+    
+    void damaged (int damage)
+    {
+        health -= damage;
+            
+    }
+    public void goFirst(string name)
+    {
+        Debug.Log(name + "선공");
+    }
+    public void goSnd(string name)
+    {
+        Debug.Log(name + "후공");
+    }
+}
